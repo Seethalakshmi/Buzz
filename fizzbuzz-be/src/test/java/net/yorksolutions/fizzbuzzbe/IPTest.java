54f84a91027b65a0c4dd0555767662015d51e60c
mfortenberry <mfortenberry@yorksolutions.net>
2022-05-26 11:16:50 -0500

    fizzbuzz tests imported

---
+++ fizzbuzz-be/src/test/java/net/yorksolutions/fizzbuzzbe/IPTest.java
@@ -0,0 +1,28 @@
+package net.yorksolutions.fizzbuzzbe;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IPTest {
+    @Test
+    void itShouldReturnAnIP() {
+        IP ip = new IP("1.2.3.4");
+//        assertEquals("1.2.3.4", ip.getIp());
+    }
+
+    @Test
+    void itShouldJsonify() throws JsonProcessingException {
+        final String ip = "1.2.3.4";
+        assertEquals(
+                "{\"ip\":\""+ip+"\"}",
+                new ObjectMapper().writeValueAsString(new IP(ip))
+        );
+    }
+}
