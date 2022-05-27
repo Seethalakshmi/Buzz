859be6918eb8dfa051feb351b504ff9329582ea6
mfortenberry <mfortenberry@yorksolutions.net>
2022-05-27 14:24:04 -0500

    redux boilerplate

--- fizzbuzz-fe/src/index.js
+++ fizzbuzz-fe/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {applyMiddleware, compose, createStore} from "redux";
+import user from './modules/user'
+import {Provider} from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const handleAsync = storeAPI => next => action => {
+    if (typeof action === 'function')
+        return action(storeAPI.dispatch, storeAPI.getState)
+
+    next(action)
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(user, composeEnhancers(applyMiddleware(handleAsync)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
