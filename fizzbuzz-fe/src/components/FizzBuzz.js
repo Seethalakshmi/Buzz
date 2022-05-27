4dfa59938d8818c2977612cb82cf106a24f82278
mfortenberry <mfortenberry@yorksolutions.net>
2022-05-27 16:04:39 -0500

    FizzBuzz component

---
+++ fizzbuzz-fe/src/components/FizzBuzz.js
@@ -0,0 +1,18 @@
+import {Form} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import {initiateFizz} from "../modules/fizzbuzz";
+
+export default function FizzBuzz({_useSelector=useSelector, _useDispatch=useDispatch, _initiateFizz=initiateFizz}) {
+    const result = _useSelector(state => state.fizzbuzz?.result)
+    const token = _useSelector(state => state.user?.token)
+    const dispatch = _useDispatch()
+
+    function handleChange(event) {
+        dispatch(_initiateFizz(token, event.target.value))
+    }
+
+    return <Form>
+        <Form.Control placeholder='Input' onChange={handleChange}/>
+        <Form.Label title='Result'>{result}</Form.Label>
+    </Form>
+}
